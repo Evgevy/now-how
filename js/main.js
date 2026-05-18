74df663516765e1670e02dba3b35eaa1ff51a8a0
unknown <djek.uait1998@gmail.com>
2026-05-18 20:31:12 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const casesSwiper = new Swiper('.cases-swiper', {
+    slidesPerView: 4,
+    spaceBetween: 172,
+    loop: true,
+    navigation: {
+        nextEl: '.case-next',
+        prevEl: '.case-prev',
+    }
+});
